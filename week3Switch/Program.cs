2234009b6454477d2f96b7065355dcc27d98a4d2
KadriGit <kadri.viira@tktk.ee>
2024-09-03 20:02:05 +0300

    Add project files.

---
+++ week3Switch/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int weekDay =Convert.ToInt32(DateTime.Now.DayOfWeek);
+Console.WriteLine(weekDay);
+
+if(weekDay == 0)
+{
+    Console.WriteLine("Sunday");
+}
+else if (weekDay ==1)
+{
+    Console.WriteLine("Monday");
+}
+else if (weekDay == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+else if (weekDay==3)
+{
+    Console.WriteLine("Wendesday");
+}
+else if(weekDay==4)
+{
+    Console.WriteLine("Thursday");
+}
+else if(weekDay==5)
+{
+    Console.WriteLine("Friday");
+}
+else
+{
+    Console.WriteLine("Saturday");
+}
